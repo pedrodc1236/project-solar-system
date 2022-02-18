fe54859f4ed8618a83da5bd6ada1a878c48952ef
pedrodc1236@gmail.com <pedrodc1236@gmail.com>
2022-02-18 17:12:46 -0300

    Requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solaar</p>);
+    return (
+      <div>
+        <Header />
+      </div>
+    );
   }
 }
 
