b1ef9a3e3744df33de767c02e108d3697d3da5de
pedrodc1236@gmail.com <pedrodc1236@gmail.com>
2022-02-19 02:26:32 -0300

    atualizando

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,9 +5,9 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div className="planet-div" data-testid="planet-card">
         <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img className="img-planet" src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
